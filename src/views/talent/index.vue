49c9c087eb3abc45569f8e69e70fb7286635a8e3
袁澧林 <kobe980918@gmail.com>
2023-11-15 17:31:10 +0800

    更新 index.vue

---
+++ src/views/talent/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { reactive } from "vue";
+import TalentList from "@/components/list/TalentList.vue";
+
+const talentList = reactive([
+  {
+    id: 1,
+  },
+  {
+    id: 2,
+  },
+])
+</script>
+
+<template>
+  <TalentList :talent-list="talentList" />
+</template>
+
+<style scoped></style>
