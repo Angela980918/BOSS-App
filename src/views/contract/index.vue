a39fb784fca38c233641e217da1302369c971a3e
袁澧林 <kobe980918@gmail.com>
2023-11-15 17:31:00 +0800

    更新 index.vue

--- src/views/contract/index.vue
+++ src/views/contract/index.vue
@@ -1,12 +1,21 @@
 <script setup lang="ts">
+import { reactive } from "vue";
 import FooterTabbar from "@/components/FooterTabbar.vue";
+import ContractList from "@/components/list/ContractList.vue";
+
+const taskList = reactive([
+  {
+    id: 1,
+  },
+  {
+    id: 2,
+  },
+]);
 </script>
 
 <template>
-  <h2>合约</h2>
+  <ContractList :contract-list="taskList" />
   <FooterTabbar />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
