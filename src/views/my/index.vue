327d95156254ea09f87a82c462c15494e905745f
袁澧林 <kobe980918@gmail.com>
2023-11-15 17:31:08 +0800

    更新 index.vue

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
 import FooterTabbar from "@/components/FooterTabbar.vue";
+
 </script>
 
 <template>
-  <h2>我的</h2>
   <FooterTabbar />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
