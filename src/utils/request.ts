749161b25eaf6b14b9bdcf5f264b34dac4e05b85
袁澧林 <kobe980918@gmail.com>
2023-11-17 17:29:37 +0800

    更新 request.ts

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,42 +1,42 @@
 import axios from "axios";
-import { showToast } from 'vant';
+import { showToast } from "vant";
 
-let baseURL = "/api"
+let baseURL = "/api";
 const service = axios.create({
-    baseURL,
-    timeout: 10000
-})
+  baseURL,
+  timeout: 10000,
+});
 
 // 发起请求之前的拦截器
 service.interceptors.request.use(
-  config => {
-    const token = window.localStorage.getItem('token')
-        if(token){
-            config.params = {
-                'token': token
-            }
-        }
-        return config
+  (config) => {
+    const token = window.localStorage.getItem("token");
+    if (token) {
+      config.headers = {
+        "x-access-token": token,
+      };
+    }
+    return config;
   },
-  error => Promise.reject(error)
-)
+  (error) => Promise.reject(error)
+);
 
 // 响应拦截器
 service.interceptors.response.use(
-  response => {
-      const res = response.data
-      if (response.status !== 200){
-          return Promise.reject(new Error(res.success || 'error'))
-      }else{
-          if (res.code == 200){
-              return res.result
-          }else{
-            showToast(res.success)
-          }
+  (response) => {
+    const res = response.data;
+    if (response.status !== 200) {
+      return Promise.reject(new Error(res.success || "error"));
+    } else {
+      if (res.code == 200) {
+        return res.result;
+      } else {
+        showToast(res.success);
       }
+    }
   },
-  error => {
-      return Promise.reject(error)
+  (error) => {
+    return Promise.reject(error);
   }
-)
-export default service
+);
+export default service;
