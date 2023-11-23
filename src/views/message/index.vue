891a20fb17a0f3cec943ad19b4f93b8c7273958a
袁澧林 <kobe980918@gmail.com>
2023-11-23 17:20:59 +0800

    更新 index.vue

--- src/views/message/index.vue
+++ src/views/message/index.vue
@@ -2,19 +2,20 @@
 import { reactive } from "vue";
 import FooterTabbar from "@/components/FooterTabbar.vue";
 import MessageList from "@/components/list/MessageList.vue";
+import { messageStore } from '@/store/message'
 
-const messageList = reactive([
-  {
-    id: 1,
-  },
-  {
-    id: 2,
-  },
-]);
+const store = messageStore()
+
+// 1.获取消息列表
+store.getSystemMessageList()
+
+// 2.获取聊天消息列表
+store.getChatMessageList()
 </script>
 
 <template>
-  <MessageList :message-list="messageList" />
+  <MessageList :message-list="store.systemMessageList" type="system" />
+  <MessageList :message-list="store.messageList" />
   <FooterTabbar />
 </template>
 
