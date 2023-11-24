6f39fca18c268dfd4921586d8c3fc3638bfc1fdc
袁澧林 <kobe980918@gmail.com>
2023-11-24 17:26:52 +0800

    表情管理

---
+++ src/views/message/components/TalkEmoji.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { inject } from 'vue'
+
+// 选中图标
+const { emojiChange } = inject('popup')
+const list = [
+  '😁', '😂', '🤣', '😃', '😄', '😅', '😆', '😉', '😊', '😋', '😎', '😍', '😘',
+  '😗', '😙', '😚', '🙂', '🤗', '🤩', '🤔', '🤨', '😐', '😑', '😶', '🙄', '😏',
+  '😣', '😥', '😮', '🤐', '😯', '😪', '😫', '😴', '😌', '😛', '😜', '😝', '🤤',
+]
+
+</script>
+<template>
+  <div>
+    <span v-for="(item, index) in list" :key="index" @click="emojiChange(item)">{{ item }}</span>
+  </div>
+</template>
+<style scoped>
+div {
+  height: 8rem;
+  margin-top: 0.64rem;
+  overflow-y: auto;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+div span {
+  width: 14.28%;
+  display: inline-block;
+  margin-bottom: 0.32rem;
+  font-size: 1.4rem;
+  text-align: center;
+}
+</style>
