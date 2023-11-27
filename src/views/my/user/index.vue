ca0f2f0eaaeb133c10b0d5a16595a1a44e7c3ea2
袁澧林 <kobe980918@gmail.com>
2023-11-27 17:26:02 +0800

    创建-->个人信息页面

---
+++ src/views/my/user/index.vue
@@ -0,0 +1,19 @@
+<!-- 个人信息-->
+<script lang="ts" setup>
+import UserInfoPage from '../components/UserInfoPage.vue'
+import { provide } from 'vue';
+
+const closeChange = () => {
+  history.back();
+}
+provide('popup', {
+  closeChange
+})
+
+</script>
+
+<template>
+  <UserInfoPage />
+</template>
+
+<style scoped></style>
