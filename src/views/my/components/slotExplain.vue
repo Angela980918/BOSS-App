68283c751d6cc1433648958a23ac570c2e4707bd
袁澧林 <kobe980918@gmail.com>
2023-12-04 17:29:29 +0800

    创建 slotExplain.vue

---
+++ src/views/my/components/slotExplain.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div class="content">
+    <slot></slot>
+    <p>最终解释权归IT企业服务平台所有</p>
+  </div>
+</template>
+<style scoped>
+.content {
+  padding: 0.77rem 0.67rem;
+}
+
+p {
+  font-size: 0.66rem;
+  color: #666666;
+  text-align: center;}
+</style>
