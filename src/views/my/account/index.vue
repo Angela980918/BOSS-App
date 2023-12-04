8a24ec28df3eebac3086dbf0cfa377d6a936a837
袁澧林 <kobe980918@gmail.com>
2023-12-04 17:28:57 +0800

    更新 index.vue

--- src/views/my/account/index.vue
+++ src/views/my/account/index.vue
@@ -1,12 +1,46 @@
 <!-- 我的账户 -->
 <script lang="ts" setup>
+import AccountInfo from '../components/AccountInfo.vue'
 
+const leftBack = () => { history.back() }
 </script>
 
 <template>
-  <div>
-    我的账户
-  </div>
+  <van-nav-bar title="我的账户" left-arrow @click-left="leftBack" />
+  <AccountInfo></AccountInfo>
 </template>
 
-<style scoped></style>
+<style scoped>
+:deep(.van-nav-bar__content) {
+  background: #FF8E00;
+  /* background: red; */
+
+}
+
+:deep(.van-nav-bar__title),
+:deep(.van-icon-arrow-left:before) {
+  color: #ffffff;
+}
+
+.van-hairline--bottom:after {
+  border: 0;
+}
+
+dl {
+  border-bottom: 1px solid #eeeeee;
+  display: flex;
+  justify-content: space-around;
+}
+
+dl dt {
+  padding: 1.47rem 0 0.59rem;
+  font-size: 0.75rem;
+  line-height: 0.75rem;
+  font-weight: 500;
+  color: #666666;
+}
+
+dl dt.active {
+  color: #FF8E00;
+  border-bottom: 1px solid #FF8E00;
+}</style>
