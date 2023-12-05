1cfdf8d87e2166f2020e7dea4c3a00093c6ac800
袁澧林 <kobe980918@gmail.com>
2023-12-05 17:23:03 +0800

    个人简历主页

---
+++ src/views/my/resume/index.vue
@@ -0,0 +1,28 @@
+<!-- 个人简历 -->
+<script lang="ts" setup>
+import PersonInfo from './components/PersonInfo.vue'
+import PersonAdvantage from './components/PersonAdvantage.vue'
+import PositionType from './components/PositionType.vue'
+import ExcellentSkill from './components/ExcellentSkill.vue'
+import WorkExperience from './components/WorkExperience.vue'
+import ProjectExperience from './components/ProjectExperience.vue'
+import EducationalExperience from './components/EducationalExperience.vue'
+
+const leftBack = () => {
+  history.back();
+}
+</script>
+
+<template>
+  <van-nav-bar title="我的简历" left-arrow @click-left="leftBack" />
+
+  <PersonInfo></PersonInfo>
+  <PersonAdvantage></PersonAdvantage>
+  <PositionType></PositionType>
+  <ExcellentSkill></ExcellentSkill>
+  <WorkExperience></WorkExperience>
+  <ProjectExperience></ProjectExperience>
+  <EducationalExperience></EducationalExperience>
+</template>
+
+<style scoped></style>
