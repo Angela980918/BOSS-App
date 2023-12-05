5a5b06488b22e11b09ac9a115e61af0d5455d316
袁澧林 <kobe980918@gmail.com>
2023-12-05 17:21:02 +0800

    我的账户

--- src/views/my/account/index.vue
+++ src/views/my/account/index.vue
@@ -1,6 +1,78 @@
 <!-- 我的账户 -->
 <script lang="ts" setup>
+import { reactive } from 'vue';
 import AccountInfo from '../components/AccountInfo.vue'
+import AccountList from '../components/AccountList.vue'
+import { transferList, incomeList } from '@/api/my'
+
+const tabs = [
+  {
+    type: 0,
+    text: '全部'
+  },
+  {
+    type: 1,
+    text: '收入'
+  },
+  {
+    type: 2,
+    text: '提现'
+  }
+]
+
+const state = reactive({
+  type: tabs[0].type,
+  loading: false,
+  list: [],
+  activeIndex: 0
+})
+
+const onChangeTab = async (value: any) => {
+  state.activeIndex++
+  state.list = []
+  if (state.type == 0) {
+    console.log("type=0")
+    await getTransferList()
+    await getIncomeList({
+      type: value
+    })
+    state.list = state.list.flat(2)
+  }
+  if (state.type == 1) {
+    console.log("type=1")
+    await getIncomeList({
+      type: value
+    })
+    state.list = state.list.flat(2)
+  }
+  if (state.type == 2) {
+    console.log("type=2")
+    await getTransferList()
+    state.list = state.list.flat(2)
+  }
+}
+
+// 收入
+const getTransferList = async () => {
+  state.loading = true
+  const res = await transferList()
+  if (res) {
+    state.list.push(res)
+  }
+  state.loading = false
+}
+
+// 提现
+const getIncomeList = async (data) => {
+  state.loading = true
+  const res = await incomeList(data)
+  if (res) {
+    state.list.push(res)
+  }
+  state.loading = false
+}
+
+onChangeTab(0)
 
 const leftBack = () => { history.back() }
 </script>
@@ -8,6 +80,16 @@ const leftBack = () => { history.back() }
 <template>
   <van-nav-bar title="我的账户" left-arrow @click-left="leftBack" />
   <AccountInfo></AccountInfo>
+
+  <van-tabs v-model:active="state.type" title-active-color="#FF9415" color="#FF9415" @change="onChangeTab">
+    <van-tab v-for="(item, index) in tabs" :key="index" :title="item.text">
+      <van-pull-refresh v-model="state.loading" @refresh="onChangeTab">
+        <AccountList :list="state.list"></AccountList>
+        <van-loading v-if="state.activeIndex === 0 && state.loading">加载中...</van-loading>
+        <div class="wy-no-data" v-if="!state.loading && state.list.length == 0">暂无数据</div>
+      </van-pull-refresh>
+    </van-tab>
+  </van-tabs>
 </template>
 
 <style scoped>
@@ -43,4 +125,5 @@ dl dt {
 dl dt.active {
   color: #FF8E00;
   border-bottom: 1px solid #FF8E00;
-}</style>
+}
+</style>
