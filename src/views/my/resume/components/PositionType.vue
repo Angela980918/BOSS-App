d17d55ce5ade4cbcffe4828b9df85eb98d9afba5
袁澧林 <kobe980918@gmail.com>
2023-12-06 17:10:26 +0800

    更新职位类型

--- src/views/my/resume/components/PositionType.vue
+++ src/views/my/resume/components/PositionType.vue
@@ -1,6 +1,24 @@
 <!-- 职位类型 -->
 <script lang="ts" setup>
-
+import { reactive, provide } from 'vue';
+import { myStore } from '@/store/my'
+import PositionTypePopup from './PositionTypePopup.vue'
+const store = myStore()
+const state = reactive({
+  show: false
+})
+const props = defineProps({
+  page: {
+    type: String
+  }
+})
+const closeChange = () => {
+  state.show = false
+  store.getResumeDetail()
+}
+provide('popup', {
+  closeChange
+})
 </script>
 
 <template>
@@ -9,7 +27,4 @@
   </div>
 </template>
 
-<style scoped>
-
-
-</style>
+<style scoped></style>
