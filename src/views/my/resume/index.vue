47c3b6785d7f1c7c878c4073c9e421a06bcaf2dd
袁澧林 <kobe980918@gmail.com>
2023-12-06 17:10:46 +0800

    更新 index.vue

--- src/views/my/resume/index.vue
+++ src/views/my/resume/index.vue
@@ -7,7 +7,10 @@ import ExcellentSkill from './components/ExcellentSkill.vue'
 import WorkExperience from './components/WorkExperience.vue'
 import ProjectExperience from './components/ProjectExperience.vue'
 import EducationalExperience from './components/EducationalExperience.vue'
+import { myStore } from '@/store/my'
 
+const store = myStore()
+store.getResumeDetail()
 const leftBack = () => {
   history.back();
 }
@@ -16,13 +19,34 @@ const leftBack = () => {
 <template>
   <van-nav-bar title="我的简历" left-arrow @click-left="leftBack" />
 
-  <PersonInfo></PersonInfo>
+  <PersonInfo :item="store.resumeInfo"></PersonInfo>
   <PersonAdvantage></PersonAdvantage>
   <PositionType></PositionType>
   <ExcellentSkill></ExcellentSkill>
   <WorkExperience></WorkExperience>
   <ProjectExperience></ProjectExperience>
   <EducationalExperience></EducationalExperience>
+  <van-button class="resume-btn" type="primary">预览简历</van-button>
 </template>
 
-<style scoped></style>
+<style scoped>
+.resume {
+  height: calc(100vh - 46px - 3rem);
+  overflow: auto;
+}
+
+.resume-btn {
+  height: 2.35rem;
+  background: linear-gradient(90deg, #FEA829, #FE8F27);
+  border-radius: 0.27rem;
+  margin: 0 0.64rem 0.64rem;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: calc(100vw - 1.28rem);
+  border: none;
+  font-size: 0.8rem;
+  font-weight: 400;
+  color: #FFFFFF;
+}
+</style>
