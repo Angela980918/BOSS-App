527888e1873414065103785157009b63c7eae3a5
袁澧林 <kobe980918@gmail.com>
2023-12-07 17:28:50 +0800

    更新 PositionType.vue

--- src/views/my/resume/components/PositionType.vue
+++ src/views/my/resume/components/PositionType.vue
@@ -14,7 +14,6 @@ const props = defineProps({
 })
 const closeChange = () => {
   state.show = false
-  store.getResumeDetail()
 }
 provide('popup', {
   closeChange
@@ -22,9 +21,18 @@ provide('popup', {
 </script>
 
 <template>
-  <div>
-    职位类型
+  <div class="resume-label">
+    <h3>求职意愿<van-icon name="add-o" @click="state.show = true" v-if="props.page !== 'preview'" /></h3>
+    <label v-if="store.resumeInfo.position_name || store.resumeInfo.service_mode || store.resumeInfo.service_price">
+      {{ store.resumeInfo.position_name }} | {{ store.resumeInfo.service_mode }} | {{ store.resumeInfo.service_price
+      }}</label>
+    <label v-else>请您填写求职意愿</label>
   </div>
+
+  <!--求职意愿弹窗-->
+  <van-popup v-model:show="state.show" position="right" :style="{ width: '100%', height: '100%' }">
+    <PositionTypePopup></PositionTypePopup>
+  </van-popup>
 </template>
 
 <style scoped></style>
