19074aa0c7f5de8f7b3ed8b56ed25190f0791bbc
袁澧林 <kobe980918@gmail.com>
2023-12-08 17:23:00 +0800

    更新 ExcellentSkill.vue

--- src/views/my/resume/components/ExcellentSkill.vue
+++ src/views/my/resume/components/ExcellentSkill.vue
@@ -1,12 +1,49 @@
 <!-- 擅长技能 -->
 <script lang="ts" setup>
+import ExcellentSkillPopup from './ExcellentSkillPopup.vue'
+import { reactive, provide } from 'vue';
+const state = reactive({
+  show: false
+})
 
+const props = defineProps({
+  page: {
+    type: String
+  }
+})
+
+const closeChange = () => {
+  state.show = false
+}
+provide('popup', {
+  closeChange
+})
 </script>
 
 <template>
-  <div>
-    擅长技能
+  <div class="resume-label">
+    <h3>擅长技能<van-icon name="add-o" @click="state.show = true" /></h3>
+
+    <label>请您填写擅长技能</label>
   </div>
+
+  <!--擅长技能弹窗-->
+  <van-popup v-model:show="state.show" position="right" :style="{ width: '100%', height: '100%' }">
+    <ExcellentSkillPopup></ExcellentSkillPopup>
+  </van-popup>
 </template>
 
-<style scoped></style>
+<style scoped>
+.text-list dt {
+  font-size: 0.59rem;
+  line-height: 0.59rem;
+  font-weight: 300;
+  color: #666666;
+  padding: 0.4rem 0.5rem;
+  margin-right: 0.27rem;
+  margin-bottom: 0.3rem;
+  background: #F6F7F8;
+  border-radius: 0.16rem;
+  display: inline-block;
+}
+</style>
